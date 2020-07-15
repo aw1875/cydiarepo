33a81cfa1b55e48a49782b08e2969e3ff8d222fb
Adam Wolf <adamwolf@Adams-Catalina-MacBook.local>
2020-07-15 18:14:09 -0400

    Testing the self-building scripts

--- update.sh
+++ update.sh
@@ -39,8 +39,58 @@ read -p "Enter Section (ex: Tweaks): " SECTION
 
 ########## Generate Depiction Page ##########
 
+echo "Generating depictions page"
+sleep 2
+
+echo "<!DOCTYPE html>" >> tweaks/$NAME.html
 echo "<html>" >> tweaks/$NAME.html
-echo "<p>$VERSION</p>" >> tweaks/$NAME.html
+echo "<head>" >> tweaks/$NAME.html
+echo "    <meta charset=\"utf-8\">" >> tweaks/$NAME.html
+echo "    <meta name=\"viewport\" content=\"width=device-width, initial-scale=1, minimum-scale=1.0, user-scalable=0\">" >> tweaks/$NAME.html
+echo "    <title>$NAME</title>" >> tweaks/$NAME.html
+echo "    <link rel=\"stylesheet\" type=\"text/css\" href=\"includes/style.css\">" >> tweaks/$NAME.html
+echo "    <style>a, .tint, .table-btn:after {color: #16d887} .active {color: #16d887;border-bottom: 5px solid #16d887;}</style>" >> tweaks/$NAME.html
+echo "    <script src=\"includes/script.js\"></script>" >> tweaks/$NAME.html
+echo "</head>" >> tweaks/$NAME.html
+echo "<body>" >> tweaks/$NAME.html
+echo "    <div class=\"body\">" >> tweaks/$NAME.html
+echo "        <div class=\"package package_head\">" >> tweaks/$NAME.html
+echo "            <img src=\" ICON \">" >> tweaks/$NAME.html
+echo "            <div class=\"package_info\">" >> tweaks/$NAME.html
+echo "                <h1 class=\"package_name\">$NAME</h1>" >> tweaks/$NAME.html
+echo "                <p class=\"package_caption\">Adam Wolf</p>" >> tweaks/$NAME.html
+echo "            </div>" >> tweaks/$NAME.html
+echo "        </div>" >> tweaks/$NAME.html
+echo "        <div class=\"nav\">" >> tweaks/$NAME.html
+echo "            <div class=\"nav_btn active tweak_info_btn\" onclick=\"swap('.changelog','.tweak_info');\">Details</div>" >> tweaks/$NAME.html
+echo "            <div class=\"nav_btn changelog_btn\" onclick=\"swap('.tweak_info','.changelog');\">Changelog</div>" >> tweaks/$NAME.html
+echo "        </div>" >> tweaks/$NAME.html
+echo "        <div class=\"tweak_info\">            " >> tweaks/$NAME.html
+echo "            <div class=\"md_view\">" >> tweaks/$NAME.html
+echo "                <p>$DESCRIPTION</p>" >> tweaks/$NAME.html
+echo "            </div>" >> tweaks/$NAME.html
+echo "            <h3>Information</h3>" >> tweaks/$NAME.html
+echo "            <div class=\"table\">" >> tweaks/$NAME.html
+echo "                <div class=\"cell\">" >> tweaks/$NAME.html
+echo "                    <div class=\"title\">Version</div>" >> tweaks/$NAME.html
+echo "                    <div class=\"text\">$VERSION</div>" >> tweaks/$NAME.html
+echo "                    <br><br>" >> tweaks/$NAME.html
+echo "                </div>" >> tweaks/$NAME.html
+echo "                <div class=\"cell\">" >> tweaks/$NAME.html
+echo "                    <div class=\"title\">Section</div>" >> tweaks/$NAME.html
+echo "                    <div class=\"text\">$SECTION</div>" >> tweaks/$NAME.html
+echo "                    <br><br>" >> tweaks/$NAME.html
+echo "                </div>" >> tweaks/$NAME.html
+echo "            </div>" >> tweaks/$NAME.html
+echo "        </div>" >> tweaks/$NAME.html
+echo "        <div class=\"changelog\">" >> tweaks/$NAME.html
+echo "            <div class=\"changelog_entry\">" >> tweaks/$NAME.html
+echo "                <h4>$VERSION</h4>" >> tweaks/$NAME.html
+echo "                <div class=\"md_view\"><p>Inital Tweak :)</p></div>" >> tweaks/$NAME.html
+echo "            </div>" >> tweaks/$NAME.html
+echo "        </div>" >> tweaks/$NAME.html
+echo "    </div>" >> tweaks/$NAME.html
+echo "</body>" >> tweaks/$NAME.html
 echo "</html>" >> tweaks/$NAME.html
 
 DEPICTION="https://repo.wolfyy.me/tweaks/$NAME.html"
